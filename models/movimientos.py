c5db430a4d70a7149ed1705cc378d1c576c12e4b
Squall-The-Crow <mzamora@sitecnet.com>
2022-06-16 04:30:59 -0500

    rpeuba3

--- models/movimientos.py
+++ models/movimientos.py
@@ -418,14 +418,14 @@ class lineas_movimientos(models.Model):
 
     movimiento_id = fields.Many2one('itriplee.movimientos', string='Movimiento')
     cantidad = fields.Integer('Cantidad')
-    estado = fields.Char('disponibilidad', default="disponible")
+
     cantidad_recibida = fields.Integer('Cantidad Recibida')
     cantidad_faltante = fields.Integer('Cantidad Faltante')
     producto = fields.Many2one('itriplee.catalogo')
     series = fields.One2many('itriplee.movimientos.series', 'movimiento', string='name')
     seriesdisponibles = fields.Many2one('itriplee.stock.series', string='Series')
     seriesdisponibles_disponibles = fields.Many2one('itriplee.stock.series', string='Series',
-    domain="[('estado', '=', disponible)]")
+    domain="[('disponible', '=', estado)]")
     estado_refaccion = fields.Selection([
                     ("nueva","Nueva"),
                     ("reparada","Reparada"),
