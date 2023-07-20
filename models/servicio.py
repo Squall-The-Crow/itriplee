fd767dd2f5ac9f4e65c634bd0916d68e9fd0089b
Squall-The-Crow <mzamora@sitecnet.com>
2023-07-20 01:52:13 -0600

    Update servicio.py

--- models/servicio.py
+++ models/servicio.py
@@ -54,7 +54,7 @@ class servicio(models.Model):
     	("Variado","Variado")],
     	 'Estado del Equipo')
     tecnico = fields.Many2one('res.users', 'Tecnico') #
-    vendedor = fields.Many2one('res.users', 'Vendedor', default=lambda self: self.cr.cliente.user_id)# 
+    vendedor = fields.Many2one('res.users', 'Vendedor', default=lambda self: self.env.cr.cliente.user_id)# 
     reinsidencia = fields.Boolean('Es reinsidencia?')
     modelo_transicion = fields.Char('Modelo Version anterior')
     garantia_asociada = fields.Many2one('itriplee.garantias', 'Garantias')
