5062bfa941684823473c464e15d741d642d81648
Squall-The-Crow <mzamora@sitecnet.com>
2023-11-08 01:53:21 -0600

    Revert "Update equipos.py"
    This reverts commit 41665ae7b3548b9b1d365fb1921c4670845dc7fe.

--- models/equipos.py
+++ models/equipos.py
@@ -11,20 +11,15 @@ class equipos(models.Model):
     factura = fields.Char('factura')
     venta = fields.Date('Fecha de Venta')
     modelo = fields.Many2one('itriplee.catalogo', 'Modelo')
-    modelo_show = fields.Char('Modelo', related='modelo.name', readonly=False, store=True)
-    catalogo = fields.Boolean('Esta en catalogo?', default=True)
-    capacidad = fields.Char('Capacidad', related='modelo.capacidad', readonly=False, store=True)
-    marca = fields.Char('Marca', related='modelo.marca.name', readonly=False, store=True)
-    tipo = fields.Char('Tipo', related='modelo.tipo.name', readonly=False, store=True)
-    tecnologia = fields.Selection('Tecnologia', related='modelo.tecnologia', readonly=False, store=True)
+    capacidad = fields.Char('Capacidad', related='modelo.capacidad', readonly=True)
+    marca = fields.Char('Marca', related='modelo.marca.name', readonly=True)
+    tipo = fields.Char('Tipo', related='modelo.tipo.name', readonly=True)
+    tecnologia = fields.Selection('Tecnologia', related='modelo.tecnologia', readonly=True)
     cliente = fields.Many2one('res.partner', 'Cliente')
     vendedor = fields.Many2one('res.users', 'Vendido Por')
     poliza = fields.Many2one('itriplee.polizas', 'Poliza')
     garantia = fields.Many2one('itriplee.garantias', 'Garantia')
     visitas = fields.Many2many ('itriplee.servicio', string='Visitas Realizadas')
-    caracteristicas = fields.Char('Caracteristicas')
-
-
 
 class equipos(models.Model):
     _name = 'itriplee.equipos_genericos'
